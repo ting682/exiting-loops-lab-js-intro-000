3a8c77d4a998c2c3aebe998f6a267741d8ed8516
Ting Chung <tchung682@gmail.com>
2020-05-13 19:46:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 function breakOut(array, changeValue, stopValue){
   for (var i = 0; i < array.length; i++) {
     debugger
-    array[i] = changeValue
+
     if(i === stopValue){
       break;
     }
+    array[i] = changeValue
+
   }
   return array;
 }
