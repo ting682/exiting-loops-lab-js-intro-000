f9bbf15d77291b91153f0659bfb38d0b563495c5
Ting Chung <tchung682@gmail.com>
2020-05-13 20:21:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function keepGoing(array, changeValue, stopValue){
 function findBy(array, findFn){
   for (var i = 0; i < array.length; i++) {
     debugger
-    console.log(findFn);
+    console.log(findFn());
     if(findFn() === array[i]){
       return array;
     }
